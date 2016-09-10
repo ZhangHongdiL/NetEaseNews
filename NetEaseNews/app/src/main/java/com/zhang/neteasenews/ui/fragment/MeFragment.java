fe47090a3bebe16a2eea7d84d1adc9a2b4b70e75
ZhangHongdiL <1205304542@qq.com>
2016-09-10 16:34:35 +0800

    Me界面的搭建

--- NetEaseNews/app/src/main/java/com/zhang/neteasenews/ui/fragment/MeFragment.java
+++ NetEaseNews/app/src/main/java/com/zhang/neteasenews/ui/fragment/MeFragment.java
@@ -1,11 +1,18 @@
 package com.zhang.neteasenews.ui.fragment;
 
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.zhang.neteasenews.R;
 
 /**
  * Created by dllo on 16/9/10.
  */
 public class MeFragment extends AbsBaseFragment {
+
+    private TextView loginTv, setTv;
+    private Button readBtn, collectBtn, replyBtn, goldBtn;
+
     @Override
     protected int setLayout() {
         return R.layout.fragment_me;
@@ -13,7 +20,12 @@ public class MeFragment extends AbsBaseFragment {
 
     @Override
     protected void initViews() {
-
+        loginTv = byView(R.id.fragment_me_login);
+        setTv = byView(R.id.fragment_me_set);
+        readBtn = byView(R.id.fragment_me_read);
+        collectBtn = byView(R.id.fragment_me_collect);
+        replyBtn = byView(R.id.fragment_me_reply);
+        goldBtn = byView(R.id.fragment_me_gold);
     }
 
     @Override
