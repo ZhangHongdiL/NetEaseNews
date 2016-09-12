7d2e116827603c549ca14591891f1c1e3591e86a
ZhangHongdiL <1205304542@qq.com>
2016-09-12 12:43:59 +0800

    个人界面的修改

---
+++ NetEaseNews/app/src/main/java/com/zhang/neteasenews/model/net/VolleyInstance.java
@@ -0,0 +1,15 @@
+package com.zhang.neteasenews.model.net;
+
+/**
+ * Created by dllo on 16/9/12.
+ * 网络请求的单例
+ */
+public class VolleyInstance {
+
+    private static VolleyInstance instance;
+
+
+    private VolleyInstance() {
+
+    }
+}
