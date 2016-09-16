111b5428c1139cad90650d836dda78c40032eb1c
ZhangHongdiL <1205304542@qq.com>
2016-09-16 15:13:33 +0800

    新闻界面的接口常量

--- NetEaseNews/app/src/main/java/com/zhang/neteasenews/ui/fragment/subfragment/ChoicenessFragment.java
+++ NetEaseNews/app/src/main/java/com/zhang/neteasenews/ui/fragment/subfragment/ChoicenessFragment.java
@@ -1,8 +1,30 @@
 package com.zhang.neteasenews.ui.fragment.subfragment;
 
+import android.content.Context;
+
+import com.zhang.neteasenews.R;
+import com.zhang.neteasenews.ui.fragment.AbsBaseFragment;
+
 /**
  * Created by dllo on 16/9/14.
  * 新闻的精选界面
  */
-public class ChoicenessFragment {
+public class ChoicenessFragment extends AbsBaseFragment {
+
+    private Context context;
+
+    @Override
+    protected int setLayout() {
+        return R.layout.fragment_news_headline;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
 }
--- NetEaseNews/app/src/main/java/com/zhang/neteasenews/utils/Values.java
+++ NetEaseNews/app/src/main/java/com/zhang/neteasenews/utils/Values.java
@@ -7,7 +7,34 @@ public class Values {
 
     // 新闻头条的接口
     public static final String HEADLINEURL = "http://c.3g.163.com/recommend/getSubDocPic?tid=T1348647909107&from=toutiao&size=20&prog=NOHEAD&offset=0&fn=2&passport=&devId=44t6%2B5mG3ACAOlQOCLuIHg%3D%3D&lat=&lon=&version=14.2&net=wifi&ts=1473231158&sign=5GqzD5yFT920iQKTNOyR3MwCzQuzy%2Buhs6NuDA7CsyF48ErR02zJ6%2FKXOnxX046I&encryption=1&canal=wandoujia_news&mac=";
-
+    // 新闻精选的接口
+    public static final String CHOICENESSURL = "http://c.m.163.com/nc/article/list/T1467284926140/0-20.html";
+    // 新闻娱乐的接口
+    public static final String AMUSEMENTURL = "http://c.m.163.com/nc/article/list/T1348648517839/0-20.html";
+    // 新闻体育的接口
+    public static final String PHYSICALURL = "http://c.m.163.com/nc/article/list/T1348649079062/0-20.html";
+    // 新闻视频的接口
+    public static final String VIDEOURL = "";
+    // 新闻财经的接口
+    public static final String FINANCEURL = "http://c.m.163.com/nc/article/list/T1348648756099/0-20.html";
+    // 新闻科技的接口
+    public static final String TECHNOLOGYURL = "http://c.m.163.com/nc/article/list/T1348649580692/0-20.html";
+    // 新闻汽车的接口
+    public static final String CARURL = "http://c.m.163.com/nc/auto/list/5YWo5Zu9/0-20.html";
+    // 新闻时尚的接口
+    public static final String FASHION = "http://c.m.163.com/nc/article/list/T1348650593803/0-20.html";
+    // 新闻图片的接口
+    public static final String PICTRUEURL = "http://c.m.163.com/photo/api/list/0096/4GJ60096.json";
+    // 新闻热帖的接口
+    public static final String HOTURL = "http://c.m.163.com/nc/article/house/5YyX5Lqs/0-20.html";
+    // 新闻房产的接口
+    public static final String HOUSEURL = "http://c.m.163.com/nc/article/house/5YyX5Lqs/0-20.html";
+    // 新闻轻松一刻的接口
+    public static final String FUNTIMEURL = "http://c.m.163.com/nc/article/list/T1350383429665/0-20.html";
+    // 新闻军事的接口
+    public static final String WARURL = "http://c.m.163.com/nc/article/list/T1348648141035/0-20.html";
+    // 新闻历史的接口
+    public static final String HISTORYURL = "http://c.m.163.com/nc/article/list/T1368497029546/0-20.html";
     // 直播的接口
     public static final String LIVEURL = "http://c.m.163.com/nc/live/list/5YWo5Zu9/0-20.html";
 
