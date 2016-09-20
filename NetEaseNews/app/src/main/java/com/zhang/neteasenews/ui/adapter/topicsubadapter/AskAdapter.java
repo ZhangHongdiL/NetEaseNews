3277ee862b0d029f80aef433d3c056e866e2d4bd
ZhangHongdiL <1205304542@qq.com>
2016-09-20 18:13:28 +0800

    话题界面的问吧

--- NetEaseNews/app/src/main/java/com/zhang/neteasenews/ui/adapter/topicsubadapter/AskAdapter.java
+++ NetEaseNews/app/src/main/java/com/zhang/neteasenews/ui/adapter/topicsubadapter/AskAdapter.java
@@ -1,13 +1,20 @@
 package com.zhang.neteasenews.ui.adapter.topicsubadapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+import com.zhang.neteasenews.R;
 import com.zhang.neteasenews.model.entity.topicsubentity.AskEntity;
 
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by dllo on 16/9/20.
@@ -16,13 +23,13 @@ import java.util.List;
 public class AskAdapter extends BaseAdapter {
 
     private Context context;
-    private List<AskEntity> datas;
+    private List<AskEntity.DataBean.ExpertListBean> datas;
 
     public AskAdapter(Context context) {
         this.context = context;
     }
 
-    public void setDatas(List<AskEntity> datas) {
+    public void setDatas(List<AskEntity.DataBean.ExpertListBean> datas) {
         this.datas = datas;
         notifyDataSetChanged();
     }
@@ -44,10 +51,41 @@ public class AskAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        AskViewHolder askViewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_fra_topic_ask_listview, parent, false);
+            askViewHolder = new AskViewHolder(convertView);
+            convertView.setTag(askViewHolder);
+        } else {
+            askViewHolder = (AskViewHolder) convertView.getTag();
+        }
+        AskEntity.DataBean.ExpertListBean expertListBean = (AskEntity.DataBean.ExpertListBean) getItem(position);
+        if (expertListBean != null) {
+            Picasso.with(context).load(expertListBean.getPicurl()).into(askViewHolder.titleImg);
+            Picasso.with(context).load(expertListBean.getHeadpicurl()).into(askViewHolder.personImg);
+            askViewHolder.contentTv.setText(expertListBean.getAlias());
+            askViewHolder.personTv.setText(expertListBean.getName());
+            askViewHolder.titleTv.setText(expertListBean.getTitle());
+            askViewHolder.sourceTv.setText(expertListBean.getClassification());
+            askViewHolder.attentionTv.setText(expertListBean.getConcernCount() + "关注");
+            askViewHolder.askTv.setText(expertListBean.getQuestionCount() + "提问");
+        }
+        return convertView;
     }
 
     class AskViewHolder {
-        
+        ImageView titleImg;
+        CircleImageView personImg;
+        TextView personTv, contentTv, sourceTv, attentionTv, askTv, titleTv;
+        public AskViewHolder(View view) {
+            titleImg = (ImageView) view.findViewById(R.id.ask_title_img);
+            personImg = (CircleImageView) view.findViewById(R.id.ask_person_img);
+            personTv = (TextView) view.findViewById(R.id.ask_person_tv);
+            contentTv = (TextView) view.findViewById(R.id.ask_content_tv);
+            sourceTv = (TextView) view.findViewById(R.id.ask_source_tv);
+            attentionTv = (TextView) view.findViewById(R.id.ask_attention_tv);
+            askTv = (TextView) view.findViewById(R.id.ask_ask_tv);
+            titleTv = (TextView) view.findViewById(R.id.ask_title_tv);
+        }
     }
 }
--- NetEaseNews/app/src/main/java/com/zhang/neteasenews/ui/fragment/topicsubfragment/AskFragment.java
+++ NetEaseNews/app/src/main/java/com/zhang/neteasenews/ui/fragment/topicsubfragment/AskFragment.java
@@ -2,16 +2,27 @@ package com.zhang.neteasenews.ui.fragment.topicsubfragment;
 
 import android.widget.ListView;
 
+import com.google.gson.Gson;
 import com.zhang.neteasenews.R;
+import com.zhang.neteasenews.model.entity.topicsubentity.AskEntity;
+import com.zhang.neteasenews.model.net.VolleyInstance;
+import com.zhang.neteasenews.model.net.VolleyResult;
+import com.zhang.neteasenews.ui.adapter.topicsubadapter.AskAdapter;
 import com.zhang.neteasenews.ui.fragment.AbsBaseFragment;
+import com.zhang.neteasenews.utils.Values;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by dllo on 16/9/10.
  * 话题界面问吧的Fragment
  */
-public class AskFragment extends AbsBaseFragment{
+public class AskFragment extends AbsBaseFragment implements VolleyResult {
 
     private ListView askLv;
+    private List<AskEntity.DataBean.ExpertListBean> datas;
+    private AskAdapter askAdapter;
 
     @Override
     protected int setLayout() {
@@ -21,11 +32,26 @@ public class AskFragment extends AbsBaseFragment{
     @Override
     protected void initViews() {
         askLv = byView(R.id.fragment_topic_ask_lv);
-
     }
 
     @Override
     protected void initDatas() {
+        datas = new ArrayList<>();
+        askAdapter = new AskAdapter(context);
+        askLv.setAdapter(askAdapter);
+        VolleyInstance.getInstance().startRequest(Values.ASKURL, this);
+    }
+
+    @Override
+    public void success(String resultStr) {
+        Gson gson = new Gson();
+        AskEntity.DataBean dataBean = gson.fromJson(resultStr, AskEntity.DataBean.class);
+        datas = dataBean.getExpertList();
+        askAdapter.setDatas(datas);
+    }
+
+    @Override
+    public void failure() {
 
     }
 }
